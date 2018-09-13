07eb7266185335a5cbd1bc601c63f730bbdb1b3d
Michael Albertz <michael@prio.one>
2018-09-13 11:43:27 +0200

    Fix some gatsby configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby SB Konzept Starter'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
+        name: 'gatsby-starter-sbkonzept',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
     },
-    'gatsby-plugin-offline',
-  ],
+    'gatsby-plugin-offline'
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import styled from 'styled-components'
 import Layout from '../components/Layout'
+
+const Test = styled.h1`
+  color: red;
+`
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <Test>Hi people</Test>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
